99dcb69caea4c12454158403039329c9056ff38b
Luis Angel Barrancos Ortiz <luisbar180492@gmail.com>
2023-12-19 23:42:54 -0400

    fix: fix scope for fetching transactions

--- financial_services/getDeposits.js
+++ financial_services/getDeposits.js
@@ -1,44 +1,36 @@
 // Importaciones
 const fetch = require('node-fetch');
-const { getGlobalIdentityId } = require('../registration_login_services/login');
 const { getUniqueUserApiKey } = require('../registration_login_services/login');
 const { transformDateCreation } = require('../formatting_services/transformDateFormat');
 
 // Obtener los depositos por IdentityId
 async function getDeposits() {
-  const existingIdentityId = getGlobalIdentityId();
   const userApiKey = getUniqueUserApiKey();
+  const apiUrl = `https://api.orangepill.cloud/v1/transactions/all?scope=-own,incoming&query={"type":"deposit"}`;
 
-  if (existingIdentityId) {
-    const apiUrl = `https://api.orangepill.cloud/v1/transactions/all?scope=-own,all&query={"type":"deposit","destination.holder":"${existingIdentityId}"}`;
+  const fetchOptions = {
+    method: 'GET',
+    headers: {
+      'x-api-key': userApiKey,
+    },
+  };
 
-    const fetchOptions = {
-      method: 'GET',
-      headers: {
-        'x-api-key': userApiKey,
-      },
-    };
+  try {
+    const response = await fetch(apiUrl, fetchOptions);
 
-    try {
-      const response = await fetch(apiUrl, fetchOptions);
-
-      if (!response.ok) {
-        throw new Error(`Error de red: ${response.status}`);
-      }
-
-      const data = await response.json();
-
-      // Utiliza el servicio para transformar la fecha de creación
-      const dataWithTransformedDate = data.map(transformDateCreation);
-
-      return dataWithTransformedDate;
-    } catch (error) {
-      console.error('Error en la solicitud FETCH:', error.message);
-      throw error; 
+    if (!response.ok) {
+      throw new Error(`Error de red: ${response.status}`);
     }
-  } else {
-    console.error('IdentityId no disponible');
-    return null;
+
+    const data = await response.json();
+
+    // Utiliza el servicio para transformar la fecha de creación
+    const dataWithTransformedDate = data.map(transformDateCreation);
+
+    return dataWithTransformedDate;
+  } catch (error) {
+    console.error('Error en la solicitud FETCH:', error.message);
+    throw error; 
   }
 }
 
--- financial_services/getSentTransactions.js
+++ financial_services/getSentTransactions.js
@@ -1,43 +1,35 @@
 // Importaciones
 const fetch = require('node-fetch');
-const { getGlobalIdentityId } = require('../registration_login_services/login');
 const { getUniqueUserApiKey } = require('../registration_login_services/login')
 const { transformDateCreation } = require('../formatting_services/transformDateFormat');
 
 // Obtener las transacciones enviadas por IdentityId
 async function getSentTransactions() {
-  const existingIdentityId = getGlobalIdentityId();
   const userApiKey = getUniqueUserApiKey();
+  const apiUrl = `https://api.orangepill.cloud/v1/transactions/all?scope=-own,outgoing&query={"type":"send"}`;
 
-  if (existingIdentityId) {
-    const apiUrl = `https://api.orangepill.cloud/v1/transactions/all?scope=-own,all&query={"type":"send","source.holder":"${existingIdentityId}"}`;
+  const fetchOptions = {
+    method: 'GET',
+    headers: {
+      'x-api-key': userApiKey,
+    },
+  };
 
-    const fetchOptions = {
-      method: 'GET',
-      headers: {
-        'x-api-key': userApiKey,
-      },
-    };
+  try {
+    const response = await fetch(apiUrl, fetchOptions);
 
-    try {
-      const response = await fetch(apiUrl, fetchOptions);
-
-      if (!response.ok) {
-        throw new Error(`Error de red: ${response.status}`);
-      }
-
-      const data = await response.json();
-
-      // Utiliza el servicio para transformar la fecha de creación
-      const dataWithTransformedDate = data.map(transformDateCreation);
-
-      return dataWithTransformedDate;
-    } catch (error) {
-      throw new Error('Error en la solicitud FETCH:', error);
+    if (!response.ok) {
+      throw new Error(`Error de red: ${response.status}`);
     }
-  } else {
-    console.error('IdentityId no disponible');
-    return null;
+
+    const data = await response.json();
+
+    // Utiliza el servicio para transformar la fecha de creación
+    const dataWithTransformedDate = data.map(transformDateCreation);
+
+    return dataWithTransformedDate;
+  } catch (error) {
+    throw new Error('Error en la solicitud FETCH:', error);
   }
 }
 
